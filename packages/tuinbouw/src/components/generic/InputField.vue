0fd23df8d819b3e7b47168e5dd1d4a3e967bc1a9
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-09-24 21:14:12 +0200

    feat: place password toggle on the end of the field

--- packages/tuinbouw/src/components/generic/InputField.vue
+++ packages/tuinbouw/src/components/generic/InputField.vue
@@ -5,38 +5,33 @@
       :for="props.label"
       >{{ props.label }}</label
     >
-    <!-- <input
-      class="border bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      :id="props.label"
-      :type="props.type"
-      :placeholder="props.placeholder"
-      :value="props.value"
-      @input="updateValue"
-    /> -->
-    <input
-      class="border bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      :id="props.label"
-      :type="props.type"
-      :placeholder="props.placeholder"
-      @input="
-        $emit('update:modelValue', ($event.target as HTMLInputElement)?.value)
-      "
-      :value="modelValue"
-    />
-    <!-- <button
-      v-if="props.type === 'password'"
-      class="border bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      @click="toggle"
-    >
-      <Eye v-if="inputType === 'password'" />
-      <EyeOff v-else />
-    </button> -->
+    <div class="relative">
+      <input
+        class="border bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        :id="props.label"
+        :type="props.type === 'password' ? inputType : props.type"
+        :placeholder="props.placeholder"
+        @input="
+          $emit('update:modelValue', ($event.target as HTMLInputElement)?.value)
+        "
+        :value="modelValue"
+      />
+      <button
+        class="absolute inset-y-0 right-0 flex items-center px-3"
+        v-if="props.type === 'password'"
+        @click="toggle"
+      >
+        <Eye v-if="inputType === 'password'" />
+        <EyeOff v-else />
+      </button>
+    </div>
     <span v-if="props.error" class="text-red-600">{{ props.error }}</span>
   </div>
 </template>
 
 <script lang="ts">
 import { Eye, EyeOff } from 'lucide-vue-next'
+import { ref } from 'vue'
 
 export default {
   props: {
@@ -65,22 +60,21 @@ export default {
     },
   },
   setup(props) {
-    // const inputType = ref('password')
-    // const value = ref(props.value)
+    const inputType = ref('password')
 
-    // const toggle = () => {
-    //   if (inputType.value === 'password') {
-    //     inputType.value = 'text'
-    //   } else {
-    //     inputType.value = 'password'
-    //   }
-    // }
+    const toggle = () => {
+      if (inputType.value === 'password') {
+        inputType.value = 'text'
+      } else {
+        inputType.value = 'password'
+      }
+    }
 
     return {
       props,
-      // inputType,
-      // value,
-      // toggle,
+
+      inputType,
+      toggle,
     }
   },
   components: {
