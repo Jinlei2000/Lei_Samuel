66edfaa172efa64a4f6b7a1c0f77d4e34bb47ca3
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-09-26 10:55:20 +0200

    basic layout and implementation of navbar

--- packages/tuinbouw/src/App.vue
+++ packages/tuinbouw/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
+    <AppHeader />
     <RouterView />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import type AppHeaderVue from '../components/generic/AppHeader.vue'
+import AppHeader from './components/generic/AppHeader.vue'
+</script>
---
+++ packages/tuinbouw/src/components/generic/AppHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <Container class="py-12"
+    ><header class="flex items-center justify-between">
+      <RouterLink
+        class="flex items-center space-x-4 hover:opacity-30 focus:outline-none focus-visible:ring-4 ring-blue-400 rounded-lg"
+        to="/"
+      >
+        <Logo class="h-10" />
+      </RouterLink>
+
+      <nav>
+        <ul class="flex items-center space-x-12">
+          <li>
+            <RouterLink
+              class="py-1 focus:outline-none focus-visible:ring-4 ring-blue-400 font-semibold tracking-wide hover:text-green-400 hover:border-green-400"
+              active-class="border-b-1 border-black"
+              to="dashboard"
+              >Home</RouterLink
+            >
+          </li>
+          <li>
+            <RouterLink
+              class="py-2 rounded-md focus:outline-none focus-visible:ring-4 ring-blue-400 font-semibold tracking-wide hover:opacity-40"
+              active-class="opacity-30"
+              to="/tuin"
+              >Mijn tuin</RouterLink
+            >
+          </li>
+          <li>
+            <RouterLink
+              class="py-2 rounded-md focus:outline-none focus-visible:ring-4 ring-blue-400 font-semibold tracking-wide hover:opacity-40"
+              active-class="opacity-30"
+              to="/profile"
+              >Profile</RouterLink
+            >
+          </li>
+          <li>
+            <button
+              class="bg-[#1A281D] rounded-full flex justify-center items-center py-3 px-6 text-white"
+            >
+              <span>
+                <!-- lucide icon -->
+                <Plus class="h-6 w-6" />
+              </span>
+              Afspraak
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header></Container
+  >
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Plus } from 'lucide-vue-next'
+
+import Container from '../wrapper/Container.vue'
+import Logo from '../Logo.vue'
+
+const user = ref<Object | null>(null)
+</script>
