a80ad665773ee1311ec8ef7d23f7c77f460913fd
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-09-26 12:45:32 +0200

    api running

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    console.log('getHello()')
+    return this.appService.getHello()
   }
 }
