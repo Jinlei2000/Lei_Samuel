ca0d9722a5cbcb8228b6db01abd3d4618a3e3a1e
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-10-02 23:06:29 +0200

    feat: use validation at update and create

--- packages/api/src/materials/dto/create-material.input.ts
+++ packages/api/src/materials/dto/create-material.input.ts
@@ -1,16 +1,30 @@
 import { InputType, Field } from '@nestjs/graphql'
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator'
 
 @InputType() // graphql
 export class CreateMaterialInput {
+  @IsString()
+  @IsNotEmpty()
   @Field() // graphql
   name: string
 
+  @IsBoolean()
+  @IsOptional()
   @Field({ defaultValue: true }) // graphql
   isAvailable: boolean
 
+  @IsString()
+  @IsOptional()
   @Field({ nullable: true }) // graphql
   personId: string
 
+  @IsInt()
   @Field() // graphql
   serialNumber: number
 }
--- packages/api/src/materials/dto/update-material.input.ts
+++ packages/api/src/materials/dto/update-material.input.ts
@@ -1,24 +1,45 @@
-import { ObjectId, ObjectIdColumn } from 'typeorm'
+import { ObjectId } from 'typeorm'
 import { CreateMaterialInput } from './create-material.input'
 import { InputType, Field, PartialType, ID } from '@nestjs/graphql'
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  isMongoId,
+} from 'class-validator'
 
 @InputType()
 export class UpdateMaterialInput extends PartialType(CreateMaterialInput) {
+  @IsNotEmpty()
   @Field(() => ID) // GraphQL type
   id: ObjectId
 
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   @Field({ nullable: true }) // GraphQL type
   name: string
 
+  @IsBoolean()
+  @IsOptional()
   @Field({ nullable: true }) // GraphQL type
   isAvailable: boolean
 
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   @Field({ nullable: true }) // GraphQL type
   personId: string
 
+  @IsBoolean()
+  @IsOptional()
   @Field({ nullable: true }) // GraphQL type
   isDefect: boolean
 
+  @IsInt()
+  @IsOptional()
   @Field({ nullable: true }) // GraphQL type
   serialNumber: number
 }
