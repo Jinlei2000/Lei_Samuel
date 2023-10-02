3b3ac52496ea475f3c1e42f478fcb37cc05adb6b
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-10-02 10:57:24 +0200

    feat: add database link in entity

--- packages/api/src/materials/entities/material.entity.ts
+++ packages/api/src/materials/entities/material.entity.ts
@@ -1,28 +1,44 @@
-import { ObjectType, Field, Int, ID } from '@nestjs/graphql'
+import { ObjectType, Field, ID } from '@nestjs/graphql'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ObjectIdColumn,
+  UpdateDateColumn,
+} from 'typeorm'
 
+@Entity() // Database link - Typeorm
 @ObjectType()
 export class Material {
+  @ObjectIdColumn() // Database link - Typeorm
   @Field(() => ID) // GraphQL type
   id: string
 
+  @Column() // Database link - Typeorm
   @Field() // GraphQL type
   name: string
 
+  @Column() // Database link - Typeorm
   @Field() // GraphQL type
   isAvailable: boolean
 
+  @Column() // Database link - Typeorm
   @Field() // GraphQL type
   personId: string
 
+  @Column() // Database link - Typeorm
   @Field() // GraphQL type
   isDefect: boolean
 
+  @Column() // Database link - Typeorm
   @Field() // GraphQL type
   serialNumber: string
 
+  @CreateDateColumn({ type: 'timestamp', nullable: true }) // Database link - Typeorm
   @Field() // GraphQL type
   createdAt: Date
 
+  @UpdateDateColumn({ type: 'timestamp', nullable: true }) // Database link - Typeorm
   @Field() // GraphQL type
   updatedAt: Date
 }
