eb88129a140448e23667c828fd47eeddf2f2e8ff
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-10-03 14:17:57 +0200

    feat: add global apollo client

--- packages/tuinbouw/src/App.vue
+++ packages/tuinbouw/src/App.vue
@@ -5,7 +5,23 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { provide } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+import { useGraphql } from './composables/useGraphql'
+
+export default {
+  components: {
+    AppHeader,
+  },
+
+  setup() {
+    const { apolloClient } = useApollo()
+  },
+}
+
+provide(DefaultApolloClient, apolloClient)
 // import type AppHeaderVue from '../components/generic/AppHeader.vue'
 import AppHeader from './components/generic/AppHeader.vue'
 </script>
---
+++ packages/tuinbouw/src/composables/useGraphql.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+
+export default () => {
+  const httpLink = createHttpLink({
+    uri: 'http://[::1]:8008/graphql',
+  })
+
+  const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  })
+
+  return {
+    apolloClient,
+  }
+}
