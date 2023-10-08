8595d56e633bd7eff2ac3cb3c576b3fd2fa4ae63
Lei <lei.jin@student.howest.be>
2023-10-08 23:59:39 +0200

    feat: seeding of staffs

--- packages/api/src/seed/seed.service.ts
+++ packages/api/src/seed/seed.service.ts
@@ -3,15 +3,19 @@ import { AppointmentsService } from 'src/appointments/appointments.service'
 import { Appointment } from 'src/appointments/entities/appointment.entity'
 import { MaterialsService } from 'src/materials/materials.service'
 import { Material } from 'src/materials/entities/material.entity'
+import { StaffsService } from 'src/staffs/staffs.service'
+import { Staff } from 'src/staffs/entities/staff.entity'
 
 import * as appointments from './data/appointments.json' // set  "resolveJsonModule": true in tsconfig.json
 import * as materials from './data/materials.json'
+import * as staffs from './data/staffs.json'
 
 @Injectable()
 export class SeedService {
   constructor(
     private appointmentsService: AppointmentsService,
     private materialsService: MaterialsService,
+    private staffsService: StaffsService,
   ) {}
 
   //#region Appointments
@@ -53,6 +57,33 @@ export class SeedService {
 
   async deleteAllMaterials(): Promise<void> {
     return this.materialsService.truncate()
+  }
+  //#endregion
+
+  //#region Staffs
+  async addStaffsFromJson(): Promise<Staff[]> {
+    let theStaffs: any[] = []
+    for (let staff of staffs) {
+      const s = new Staff()
+      s.firstname = staff.firstname.toLowerCase()
+      s.lastname = staff.lastname.toLowerCase()
+      s.fullname = `${staff.firstname.toLowerCase()} ${staff.lastname.toLowerCase()}`
+      s.email = staff.email
+      s.absentCount = 0
+      s.availability = true
+      s.isAdmin = staff.isAdmin ? staff.isAdmin : false
+      s.uid = staff.uid
+      //TODO: How to add locationId here? Make a Location.
+      // s.locationId = staff.locationId
+
+      theStaffs.push(s)
+    }
+
+    return this.staffsService.saveAll(theStaffs)
+  }
+
+  async deleteAllStaffs(): Promise<void> {
+    return this.staffsService.truncate()
   }
   //#endregion
 }
