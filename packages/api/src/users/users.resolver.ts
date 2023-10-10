3c324dd427322a283ef30b98e65c49f9d23c6005
Lei <lei.jin@student.howest.be>
2023-10-10 16:20:33 +0200

    feat: user

--- packages/api/src/users/users.resolver.ts
+++ packages/api/src/users/users.resolver.ts
@@ -1,35 +1,47 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
+import { UsersService } from './users.service'
+import { Role, User } from './entities/user.entity'
+import { CreateUserInput } from './dto/create-user.input'
+import { UpdateUserInput } from './dto/update-user.input'
+import { UseGuards } from '@nestjs/common'
+import { FirebaseGuard } from 'src/authentication/guards/firebase.guard'
+import { FirebaseUser } from 'src/authentication/decorators/user.decorator'
+import { UserRecord } from 'firebase-admin/auth'
+import { RolesGuard } from './guards/roles.guard'
+import { AllowedRoles } from './decorators/role.decorator'
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(FirebaseGuard) //TODO: dont need this because of we make a client only here
   @Mutation(() => User)
-  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
-    return this.usersService.create(createUserInput);
+  createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+    @FirebaseUser() user: UserRecord,
+  ) {
+    return this.usersService.create(user.uid, createUserInput)
   }
 
+  @AllowedRoles(Role.ADMIN)
+  @UseGuards(FirebaseGuard, RolesGuard)
   @Query(() => [User], { name: 'users' })
   findAll() {
-    return this.usersService.findAll();
+    return this.usersService.findAll()
   }
 
   @Query(() => User, { name: 'user' })
   findOne(@Args('id', { type: () => String }) id: string) {
-    return this.usersService.findOne(id);
+    return this.usersService.findOne(id)
   }
 
   @Mutation(() => User)
   updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
+    return this.usersService.update(updateUserInput.id, updateUserInput)
   }
 
   @Mutation(() => User)
   removeUser(@Args('id', { type: () => String }) id: string) {
-    return this.usersService.remove(id);
+    return this.usersService.remove(id)
   }
 }
--- packages/api/src/users/users.service.ts
+++ packages/api/src/users/users.service.ts
@@ -1,32 +1,43 @@
-import { Injectable, UseGuards } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
-import { FirebaseGuard } from 'src/authentication/guards/firebase.guard';
+import { Injectable } from '@nestjs/common'
+import { CreateUserInput } from './dto/create-user.input'
+import { UpdateUserInput } from './dto/update-user.input'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Role, User } from './entities/user.entity'
 
 @Injectable()
 export class UsersService {
   constructor(
-    private readonly usersService: UsersService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  @UseGuards(FirebaseGuard)
-  create(createUserInput: CreateUserInput) {
-    return new Error('This action adds a new user');
+  // TODO: check if user exists
+  create(uid: string, createUserInput: CreateUserInput) {
+    const user = new User()
+    user.uid = uid
+    user.locale = createUserInput.locale ?? 'nl'
+    user.role = Role.CLIENT // BUG: default columns doenst seem to work.
+    return this.userRepository.save(user)
   }
 
   findAll() {
-    return new Error(`This action returns all users`);
+    return this.userRepository.find()
   }
 
   findOne(id: string) {
-    return new Error(`This action returns a #${id} user`);
+    return new Error(`This action returns a #${id} user`)
+  }
+
+  findOneByUid(uid: string) {
+    return this.userRepository.findOneByOrFail({ uid })
   }
 
   update(id: string, updateUserInput: UpdateUserInput) {
-    return new Error(`This action updates a #${id} user`);
+    return new Error(`This action updates a #${id} user`)
   }
 
   remove(id: string) {
-    return new Error(`This action removes a #${id} user`);
+    return new Error(`This action removes a #${id} user`)
   }
 }
