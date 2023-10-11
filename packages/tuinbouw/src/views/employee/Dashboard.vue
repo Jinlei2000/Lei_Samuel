9d95182bde3b10713615edc93f8734453cf801e6
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-10-11 17:15:55 +0200

    feat: schedule controls styling done

--- packages/tuinbouw/src/views/employee/Dashboard.vue
+++ packages/tuinbouw/src/views/employee/Dashboard.vue
@@ -7,11 +7,43 @@
     </h1>
     <div class="grid grid-cols-4 mx-32">
       <div class="col-start-1 col-span-1">
-        <AppointmentCard
+        <h2 class="mb-3 text-2xl">Next Client</h2>
+        <!-- for each client -->
+        <div class="flex flex-col gap-3">
+          <AppointmentCard
+            v-for="item in clients"
+            :title="item.name"
+            :description="item.description"
+            :type="item.type"
+          />
+        </div>
+        <div class="flex justify-between items-center mb-3 mt-6">
+          <h2 class="text-2xl">Schedule</h2>
+          <button class="flex text-base text-primary-orange">
+            Calendar <ChevronRight stroke-width="1" />
+          </button>
+        </div>
+
+        <div
+          class="bg-gray-500 p-1 flex justify-between items-center rounded-2xl"
+        >
+          <button
+            class="bg-primary-orange p-1 rounded-xl hover:scale-110 transition-all"
+          >
+            <ArrowLeft class="text-white" />
+          </button>
+          <p class="">Today</p>
+          <button
+            class="bg-primary-orange p-1 rounded-xl hover:scale-110 transition-all"
+          >
+            <ArrowRight class="text-white" />
+          </button>
+        </div>
+        <!-- <AppointmentCard
           title="Mr. Johnsson"
           description="Dit is een afspraak voor het snoeien van een berk."
           type="inspection"
-        />
+        /> -->
       </div>
     </div>
   </div>
@@ -19,4 +51,36 @@
 
 <script setup lang="ts">
 import AppointmentCard from '@/components/generic/AppointmentCard.vue'
+import { ArrowLeft, ArrowRight, ChevronRight } from 'lucide-vue-next'
+
+const clients = [
+  {
+    name: 'Appointment 1',
+    date: '2023-09-27T00:00:00.000Z',
+    location: 'Room A',
+    description: '2 berken snoeien',
+    type: 'repair',
+  },
+  {
+    name: 'Appointment 2',
+    date: '2023-09-28T00:00:00.000Z',
+    location: 'Room B',
+    description: 'Grasveld maaien',
+    type: 'maintenance',
+  },
+  {
+    name: 'Appointment 3',
+    date: '2023-09-29T00:00:00.000Z',
+    location: 'Room C',
+    description: 'Onderhoud aan zwemvijver',
+    type: 'maintenance',
+  },
+  {
+    name: 'Appointment 4',
+    date: '2023-09-30T00:00:00.000Z',
+    location: 'Room D',
+    description: 'Beplanting aanleggen',
+    type: 'inspection',
+  },
+]
 </script>
