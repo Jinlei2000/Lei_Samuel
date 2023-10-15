ba50c2492e1b85c22f4d8d16fcfdb6f6a91c5024
Lei <lei.jin@student.howest.be>
2023-10-15 13:42:46 +0200

    feat: export & import absences

--- packages/api/src/absences/absences.module.ts
+++ packages/api/src/absences/absences.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AbsencesService } from './absences.service';
-import { AbsencesResolver } from './absences.resolver';
+import { Module } from '@nestjs/common'
+import { AbsencesService } from './absences.service'
+import { AbsencesResolver } from './absences.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Absence } from './entities/absence.entity'
 
 @Module({
   providers: [AbsencesResolver, AbsencesService],
+
+  imports: [TypeOrmModule.forFeature([Absence])],
+  exports: [AbsencesService],
 })
 export class AbsencesModule {}
