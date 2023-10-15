f7618e6ac05d321063467ec248a61cb34b4dbffe
Lei <lei.jin@student.howest.be>
2023-10-15 14:08:14 +0200

    feat: Absence entity

--- packages/api/src/absences/entities/absence.entity.ts
+++ packages/api/src/absences/entities/absence.entity.ts
@@ -1,7 +1,45 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ObjectId,
+  ObjectIdColumn,
+  UpdateDateColumn,
+} from 'typeorm'
 
-@ObjectType()
+@Entity()
+@ObjectType({ description: 'Absence' })
 export class Absence {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @ObjectIdColumn()
+  @Field(() => ID)
+  id: ObjectId
+
+  @Column()
+  @Field({ description: 'person of absence' })
+  personId: string
+
+  @Column()
+  @Field({ nullable: true })
+  discription?: string
+
+  @Column()
+  @Field({ description: 'Type of absence (sick, vacation, other)' })
+  type: string // sick, vacation, other
+
+  @Column()
+  @Field({ description: 'Start date of absence' })
+  startDate: Date
+
+  @Column()
+  @Field({ description: 'End date of absence' })
+  endDate: Date
+
+  @CreateDateColumn({ type: 'timestamp', nullable: true }) // Database link - Typeorm
+  @Field({ nullable: true }) // GraphQL type
+  createdAt?: Date
+
+  @UpdateDateColumn({ type: 'timestamp', nullable: true }) // Database link - Typeorm
+  @Field({ nullable: true }) // GraphQL type
+  updatedAt?: Date
 }
