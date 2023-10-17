b33f6bc074886d7567678195c715f439c011a784
Lei <lei.jin@student.howest.be>
2023-10-17 11:39:22 +0200

    feat: entity

--- packages/api/src/appointments/entities/appointment.entity.ts
+++ packages/api/src/appointments/entities/appointment.entity.ts
@@ -1,7 +1,53 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql'
+import {
+  ObjectIdColumn,
+  ObjectId,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+} from 'typeorm'
 
+@Entity()
 @ObjectType()
 export class Appointment {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @ObjectIdColumn()
+  @Field(() => ID)
+  id: ObjectId
+
+  @Column()
+  @Field(() => Location)
+  location: Location
+
+  @Column()
+  @Field()
+  price: number
+
+  @Column()
+  @Field()
+  startProposedDate: Date
+
+  @Column()
+  @Field()
+  endProposedDate: Date
+
+  @Column()
+  @Field()
+  finalDate: Date
+
+  @Column()
+  @Field()
+  isScheduled: boolean
+
+  @Column()
+  @Field()
+  isDone: boolean
+
+  @CreateDateColumn({ type: 'timestamp', nullable: true })
+  @Field({ nullable: true })
+  createdAt?: Date
+
+  @UpdateDateColumn({ type: 'timestamp', nullable: true })
+  @Field({ nullable: true })
+  updatedAt?: Date
 }
