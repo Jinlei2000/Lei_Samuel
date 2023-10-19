c1fd12258341892bbd86b0203d56e70b3ec94b8a
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-10-19 14:41:34 +0200

    feat: styled filterbutton

--- packages/tuinbouw/src/views/employee/Materials.vue
+++ packages/tuinbouw/src/views/employee/Materials.vue
@@ -4,28 +4,25 @@
       <!-- filters + searchbar -->
       <div class="flex items-center gap-3">
         <button
-          class="bg-primary-orange p-1 rounded-xl hover:scale-110 transition-all"
+          class="bg-transparent p-3 h-12 rounded-2xl flex items-center gap-[6px] border-black border-1 text-black"
         >
-          <Filter />
-        </button>
-        <button
-          class="bg-primary-orange p-1 rounded-xl hover:scale-110 transition-all"
-        >
-          <Sort />
+          <Filter class="h-5 w-5" />
+          <p class="m-0 text-lg">Filter</p>
+          <ChevronDown class="h-[22px] w-[22px]" />
         </button>
       </div>
       <div class="flex items-center justify-center gap-3 relative w-1/3">
+        <button
+          class="bg-transparent p-2 rounded-full hover:scale-110 transition-all absolute left-2"
+        >
+          <Search class="h-[22px] w-[22px]" />
+        </button>
         <input
           v-model="search"
-          class="bg-gray-200 rounded-full px-3 py-2 w-full"
+          class="bg-gray-200 rounded-full h-12 py-3 pl-11 w-full text-lg"
           type="text"
           placeholder="Search for materials"
         />
-        <button
-          class="bg-transparent p-2 rounded-full hover:scale-110 transition-all absolute right-1"
-        >
-          <Search class="h-5 w-5" />
-        </button>
       </div>
     </div>
     <template v-if="search" class="grid grid-cols-4 gap-3">
@@ -81,7 +78,7 @@ import {
   FIND_MATERIALS_BY_SEARCH_STRING,
 } from '@/graphql/material.query'
 import { watch, ref } from 'vue'
-import { Filter, Search } from 'lucide-vue-next'
+import { Filter, Search, ChevronDown } from 'lucide-vue-next'
 
 const search = ref('')
 
