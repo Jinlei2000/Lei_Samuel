5a847b988e31d83fdd9b1a1389fb0f5f62219db9
Lei <lei.jin@student.howest.be>
2023-10-27 14:36:27 +0200

    feat: register page

--- packages/tuinbouw/src/views/auth/Register.vue
+++ packages/tuinbouw/src/views/auth/Register.vue
@@ -1,71 +1,62 @@
 <template>
-  <section class="bg-gray-50 dark:bg-gray-900">
+  <section class="bg-gray-50">
     <div
       class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
     >
-      <a
-        href="#"
-        class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-      >
-        <img class="w-8 h-8 mr-2" src="" alt="logo" />
-        Name
-      </a>
-      <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
-      >
+      <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1
-            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl"
           >
             Create an account
           </h1>
-          <span v-if="errorMessages.general" class="text-red-600">{{
-            errorMessages.general
+          <span v-if="errorRegister" class="text-red-600">{{
+            errorRegister
           }}</span>
-          <form @submit.prevent="handleRegister" class="space-y-4 md:space-y-6">
-            <InputField
-              label="First name"
-              type="text"
+          <form @submit.prevent="handleRegister">
+            <InputText
+              name="First name"
               placeholder="John"
-              :error="errorMessages.firstName"
-              v-model="registerCredentials.firstName"
-            />
-            <InputField
-              label="Last name"
               type="text"
-              placeholder="Doe"
-              :error="errorMessages.lastName"
-              v-model="registerCredentials.lastName"
+              :errorMessage="errorMessages.firstName"
+              v-bind="firstName"
             />
-
-            <InputField
-              label="Email"
-              type="email"
+            <InputText
+              name="Last name"
+              placeholder="John"
+              type="text"
+              :errorMessage="errorMessages.lastName"
+              v-bind="lastName"
+            />
+            <InputText
+              name="Email"
               placeholder="john@example.com"
-              :error="errorMessages.email"
-              v-model="registerCredentials.email"
+              type="text"
+              :errorMessage="errorMessages.email"
+              v-bind="email"
             />
-            <InputField
-              label="Password"
-              type="password"
+            <InputText
+              name="Password"
               placeholder="••••••••"
-              :error="errorMessages.password"
-              v-model="registerCredentials.password"
+              type="password"
+              inputType="password"
+              :errorMessage="errorMessages.password"
+              v-bind="password"
             />
-
             <button
               type="submit"
-              class="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              class="w-full text-white bg-green-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
               Create an account
             </button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+            <p class="text-sm font-light text-gray-500 pt-4">
               Already have an account?
               <RouterLink
                 to="/auth/login"
-                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >Login here</RouterLink
+                class="font-medium text-primary-600 hover:underline"
               >
+                Login
+              </RouterLink>
             </p>
           </form>
         </div>
@@ -74,129 +65,86 @@
   </section>
 </template>
 
-<script lang="ts">
-import { ref } from 'vue'
-
-import { ADD_CLIENT } from '@/graphql/user.mutation'
-import { object, string } from 'yup'
-import { useI18n } from 'vue-i18n'
-import { useMutation } from '@vue/apollo-composable'
-import InputField from '@/components/generic/form/InputField.vue'
-import router from '@/router'
-import type { CustomUser } from '@/interfaces/custom.user.interface'
-import useCustomUser from '@/composables/useCustomUser'
+<script setup lang="ts">
+import { ref, watch } from 'vue'
 import useFirebase from '@/composables/useFirebase'
+import router from '@/router'
+import useCustomUser from '@/composables/useCustomUser'
+import InputText from '@/components/generic/form/InputText.vue'
+import { useForm } from 'vee-validate'
+import * as yup from 'yup'
+import { CREATE_CLIENT } from '@/graphql/user.mutation'
+import type { CustomUser } from '@/interfaces/custom.user.interface'
+import { useMutation } from '@vue/apollo-composable'
+import { useI18n } from 'vue-i18n'
 
-export default {
-  setup() {
-    // Composables
-    const { register } = useFirebase()
-    const { customUser } = useCustomUser()
-    const { locale } = useI18n()
+// Composables
+const { register } = useFirebase()
+const { customUser } = useCustomUser()
+const { mutate: addClient, error: addClientError } =
+  useMutation<CustomUser>(CREATE_CLIENT)
+const { locale } = useI18n()
 
-    // Data
-    const registerCredentials = ref({
-      email: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-    })
-    const errorMessages = ref<{ [key: string]: string }>({
-      email: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-      general: '',
-    })
+watch(addClientError, () => {
+  if (!addClientError.value) return
+  errorRegister.value = "Couldn't create user."
+})
 
-    const {
-      mutate: addClient,
-      loading: addClientLoading,
-      onDone: clientCreated,
-    } = useMutation<CustomUser>(ADD_CLIENT)
+const schema = yup.object({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+})
+const { resetForm, defineComponentBinds, errors, values, validate } = useForm({
+  validationSchema: schema,
+})
+const email = defineComponentBinds('email')
+const password = defineComponentBinds('password')
+const firstName = defineComponentBinds('firstName')
+const lastName = defineComponentBinds('lastName')
+const errorRegister = ref<string | null>(null)
+const errorMessages = ref<{
+  [key: string]: string | undefined
+}>({
+  email: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+})
 
-    // Validation schema
-    const registerSchema = object({
-      email: string().required('email is required'),
-      password: string()
-        .required('password is required')
-        .min(6, "Password can't be shorter than 6 characters"),
-      firstName: string().required('first name is required'),
-      lastName: string().required('last name is required'),
-    })
+const handleRegister = async () => {
+  await validate()
+  errorMessages.value = errors.value
+  errorRegister.value = null
+  console.log(values)
+  if (Object.keys(errors.value).length === 0) {
+    register(values.email, values.password)
+      .then(async userData => {
+        // Add user to database
+        await addClient({
+          createClientInput: {
+            uid: userData.uid,
+            firstname: values.firstName,
+            lastname: values.lastName,
+            email: values.email,
+            // add the user's locale language to the database
+            locale: locale.value,
+          },
+        }).then(result => {
+          if (!result?.data) throw new Error('Custom user creation failed.')
 
-    // Reset error messages to empty strings
-    const resetErrorMessages = () => {
-      errorMessages.value = {
-        email: '',
-        password: '',
-        firstName: '',
-        lastName: '',
-        general: '',
-      }
-    }
+          customUser.value = result.data
 
-    // Handle validation errors
-    const handleValidationErrors = (err: any) => {
-      // console.log(err)
-      err.inner.forEach((e: { path: string; message: string }) => {
-        errorMessages.value[e.path] = e.message
+          console.log('register success')
+          resetForm()
+          router.replace('/auth/login')
+        })
       })
-      // console.log(errorMessages.value)
-    }
-
-    // Validate and register
-    const handleRegister = async () => {
-      resetErrorMessages()
-
-      // Validate login credentials with yup
-      registerSchema
-        .validate(registerCredentials.value, {
-          abortEarly: false,
-        })
-        .then(() => {
-          console.log('validation success')
-          register(
-            registerCredentials.value.email,
-            registerCredentials.value.password,
-          )
-            .then(async () => {
-              // Add user to database
-              await addClient({
-                createClientInput: {
-                  firstname: registerCredentials.value.firstName,
-                  lastname: registerCredentials.value.lastName,
-                  email: registerCredentials.value.email,
-                  // add the user's locale language to the database
-                  locale: locale.value,
-                },
-              }).then(result => {
-                if (!result?.data)
-                  throw new Error('Custom user creation failed.')
-
-                customUser.value = result.data
-
-                console.log('register success')
-                router.push('/auth/login')
-              })
-            })
-            .catch(error => {
-              console.log(error.message)
-              // TODO: handle error messages better
-              errorMessages.value.general = error.message
-            })
-        })
-        .catch(err => {
-          handleValidationErrors(err)
-        })
-    }
-
-    return {
-      registerCredentials,
-      handleRegister,
-      errorMessages,
-    }
-  },
-  components: { InputField },
+      .catch(error => {
+        console.log(error.message)
+        errorRegister.value = error.message
+      })
+  }
 }
 </script>
