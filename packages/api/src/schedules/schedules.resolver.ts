b4f529769612acf0b1e7e8866f00f2f6a2f12b61
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-10-30 08:41:45 +0100

    feat: added guards and roles to findtodayschedulebydateanduserid

--- packages/api/src/schedules/schedules.resolver.ts
+++ packages/api/src/schedules/schedules.resolver.ts
@@ -9,10 +9,15 @@ import {
 import { SchedulesService } from './schedules.service'
 import { Schedule } from './entities/schedule.entity'
 import { CreateScheduleInput } from './dto/create-schedule.input'
+import { UseGuards } from '@nestjs/common'
+import { FirebaseGuard } from 'src/authentication/guards/firebase.guard'
 import { UpdateScheduleInput } from './dto/update-schedule.input'
 import { AppointmentsService } from 'src/appointments/appointments.service'
 import { Appointment } from 'src/appointments/entities/appointment.entity'
 import { OrderByInput } from 'src/interfaces/order.input'
+import { AllowedRoles } from 'src/users/decorators/role.decorator'
+import { Role, User } from 'src/users/entities/user.entity'
+import { RolesGuard } from 'src/users/guards/roles.guard'
 
 @Resolver(() => Schedule)
 export class SchedulesResolver {
@@ -36,13 +41,17 @@ export class SchedulesResolver {
     return this.schedulesService.findOne(id)
   }
 
-  @Query(() => Schedule, { name: 'scheduleByDateAndUserId' })
-  findOneByDateAndUserId(
+  // Get schedule for today by employeeId
+  @AllowedRoles(Role.ADMIN, Role.EMPLOYEE)
+  @UseGuards(FirebaseGuard, RolesGuard)
+  @Query(() => [Schedule], { name: 'scheduleByDateAndUserId' })
+  findTodayScheduleByDateAndUserId(
     @Args('date', { type: () => String }) date: string,
     @Args('userId', { type: () => String }) userId: string,
   ) {
-    return this.schedulesService.findOneByDateAndUserId(userId, date)
+    return this.schedulesService.findTodayScheduleByDateAndUserId(userId, date)
   }
+
 
   @Mutation(() => Schedule)
   createSchedule(
