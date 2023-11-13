42911d8ee315b23df41576c031e1d035316f8b89
Lei <lei.jin@student.howest.be>
2023-11-13 16:18:43 +0100

    feat: use validation schema

---
+++ packages/tuinbouw/src/validation/schema.ts
@@ -0,0 +1,33 @@
+import * as yup from 'yup'
+
+export const absenceSchema = yup.object({
+  startDate: yup.string().required(),
+  endDate: yup.string().required(),
+  type: yup.string().required(),
+  description: yup.string().optional(),
+})
+
+export const userUpdateSchema = yup.object({
+  email: yup.string().required().email(),
+  firstname: yup.string().required(),
+  lastname: yup.string().required(),
+  telephone: yup
+    .string()
+    .matches(/^[0-9]+$/, 'Must be only digits')
+    .min(10)
+    .max(10)
+    .optional()
+    .nullable(),
+  invoiceOption: yup.string().optional().nullable(),
+  company: yup.boolean().optional().nullable(),
+  btwNumber: yup
+    .string()
+    .matches(/^[A-Za-z\d]{5,15}$/, 'Not a valid VAT Number')
+    .optional()
+    .nullable(),
+})
+
+export const locationCreateSchema = yup.object({
+  searchAdressInput: yup.string().required().trim(),
+  selectedAddress: yup.object().required(),
+})
