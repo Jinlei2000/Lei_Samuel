018d729fa22fd303a9a73d92dfdcb265c29eb09b
Lei <lei.jin@student.howest.be>
2023-11-13 16:33:48 +0100

    feat: go to google maps

--- packages/tuinbouw/src/components/generic/AppointmentCard.vue
+++ packages/tuinbouw/src/components/generic/AppointmentCard.vue
@@ -1,34 +1,39 @@
 <template>
   <div
-    v-if="props.appointment"
+    v-if="appointment"
     @click="openModal()"
     class="relative max-h-32 w-full overflow-hidden rounded-2xl bg-gray-200 p-3 pl-6"
-    :class="props.appointment.isDone ? 'opacity-50' : ''"
+    :class="appointment.isDone ? 'opacity-50' : ''"
   >
     <div
       class="absolute left-0 top-0 h-full w-1"
       :class="
-        props.appointment?.type === 'maintenance'
+        appointment?.type === 'maintenance'
           ? 'bg-primary-green'
-          : props.appointment?.type === 'repair'
+          : appointment?.type === 'repair'
           ? 'bg-primary-orange'
-          : props.appointment?.type === 'inspection'
+          : appointment?.type === 'inspection'
           ? 'bg-primary-blue'
           : 'bg-transparent'
       "
     ></div>
-    <h2 class="mb-1 text-xl">{{ props.appointment.user?.fullname }}</h2>
+    <h2 class="mb-1 text-xl">{{ appointment.user?.fullname }}</h2>
     <div class="flex items-end justify-between gap-3">
       <p class="overflow-hidden text-base">
-        {{ props.appointment.description }}
+        {{ appointment.description }}
       </p>
-      <button @click="navigateToLocation(props.appointment.location!.address)" v-on:click.stop
+      <button
+        @click="navigateToLocation(appointment.location!)"
+        v-on:click.stop
         class="bg-primary-orange flex h-fit items-center gap-2 rounded-[8px] py-[6px] pl-3 pr-[7px] text-gray-200"
       >
         Navigate <Navigation stroke-width="2" class="h-[17px] w-[17px]" />
       </button>
     </div>
-    <button @click="openModal()" class="absolute right-3 top-3 transition-all hover:scale-105">
+    <button
+      @click="openModal()"
+      class="absolute right-3 top-3 transition-all hover:scale-105"
+    >
       <Info class="h-[24px] w-[24px]" />
     </button>
   </div>
@@ -36,44 +41,53 @@
   <Dialog
     v-model:visible="showModal"
     modal
-    :header="props.appointment.user!.fullname"
-    :style="{ width: '50vw', position: 'relative', overflow: 'hidden'}"
-    v-if="props.appointment"
+    :header="appointment.user!.fullname"
+    :style="{ width: '50vw', position: 'relative', overflow: 'hidden' }"
+    v-if="appointment"
     @click:close="closeModal"
     class="max-w-lg"
   >
-  <div
+    <div
       class="absolute left-0 top-0 h-full w-1"
       :class="
-        props.appointment.type === 'maintenance'
+        appointment.type === 'maintenance'
           ? 'bg-primary-green'
-          : props.appointment.type === 'repair'
+          : appointment.type === 'repair'
           ? 'bg-primary-orange'
-          : props.appointment.type === 'inspection'
+          : appointment.type === 'inspection'
           ? 'bg-primary-blue'
           : 'bg-transparent'
       "
     ></div>
     <div class="flex flex-col gap-3">
       <p class="text-gray-900">
-        {{ props.appointment.description }}
+        {{ appointment.description }}
       </p>
-      <p class="capitalize" :class="props.appointment.type == 'maintenance' ? 'text-primary-green' : props.appointment.type == 'repair' ? 'text-primary-orange' : ''">
-        {{ props.appointment.type }}
+      <p
+        class="capitalize"
+        :class="
+          appointment.type == 'maintenance'
+            ? 'text-primary-green'
+            : appointment.type == 'repair'
+            ? 'text-primary-orange'
+            : ''
+        "
+      >
+        {{ appointment.type }}
       </p>
     </div>
-    
+
     <div class="my-6 flex flex-col gap-3">
       <div class="flex gap-3">
         <Clock />
         <p class="">
-          {{ props.appointment.finalDate!.substring(11, 16) }}
+          {{ appointment.finalDate!.substring(11, 16) }}
         </p>
       </div>
       <div class="flex gap-3">
         <MapPin />
         <p class="">
-          {{ props.appointment.location!.address }}
+          {{ appointment.location!.address }}
         </p>
       </div>
     </div>
@@ -89,7 +103,12 @@
         class="bg-primary-green flex h-fit items-center gap-2 rounded-[4px] py-[6px] pl-3 pr-[7px] text-gray-200"
         :class="appointmentIsDone ? 'bg-primary-orange' : 'bg-primary-green'"
       >
-        {{ appointmentIsDone ? 'Unfinish' : 'Finish' }}<XCircle v-if="appointmentIsDone" stroke-width="2" class="h-[17px] w-[17px]" /><CheckCircle v-else stroke-width="2" class="h-[17px] w-[17px]" />
+        {{ appointmentIsDone ? 'Unfinish' : 'Finish'
+        }}<XCircle
+          v-if="appointmentIsDone"
+          stroke-width="2"
+          class="h-[17px] w-[17px]"
+        /><CheckCircle v-else stroke-width="2" class="h-[17px] w-[17px]" />
       </button>
     </div>
   </Dialog>
@@ -100,9 +119,10 @@
 import { Navigation, Info } from 'lucide-vue-next'
 import { Clock, MapPin, CheckCircle, XCircle } from 'lucide-vue-next'
 import type { Appointment } from '@/interfaces/appointment.user.interface'
-import { ref, type PropType } from 'vue';
-import { UPDATE_APPOINTMENT } from '@/graphql/appointment.mutation';
+import { ref, type PropType } from 'vue'
+import { UPDATE_APPOINTMENT } from '@/graphql/appointment.mutation'
 import { useMutation } from '@vue/apollo-composable'
+import type { Location } from '@/interfaces/location.interface'
 
 const { mutate: updateAppointment, error: updateAppointmentError } =
   useMutation<Appointment>(UPDATE_APPOINTMENT)
@@ -129,7 +149,7 @@ const handleAppointmentUpdate = () => {
   updateAppointment({
     updateAppointmentInput: {
       id: props.appointment!.id,
-      isDone: !appointmentIsDone.value
+      isDone: !appointmentIsDone.value,
     },
   })
   appointmentIsDone.value = !appointmentIsDone.value
@@ -137,16 +157,9 @@ const handleAppointmentUpdate = () => {
   showModal.value = false
 }
 
-// TODO: add support for appointment location
 // Open location in google maps
-const navigateToLocation = (location: any) => {
-  console.log(location)
-  const latitude = 37.7749; // Replace with the latitude of your location
-  const longitude = -122.4194; // Replace with the longitude of your location
-  
-  const googleMapsUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
-  window.open(googleMapsUrl, '_blank');
+const navigateToLocation = (location: Location) => {
+  const googleMapsUrl = `https://www.google.com/maps?q=${location.lat},${location.lng}`
+  window.open(googleMapsUrl, '_blank')
 }
-
-
 </script>
