bb5a1c015d7a83e9c067b56260c4af5d3605b81d
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-11-20 14:17:49 +0100

    feat: profile page head

--- packages/tuinbouw/src/components/Profile.vue
+++ packages/tuinbouw/src/components/Profile.vue
@@ -1,137 +1,150 @@
 <template>
-  <!-- go back button -->
-  <button class="flex" @click="$router.go(-1)" v-bind="$attrs">
-    <ArrowLeft class="h-6 w-6" />
-    Go back
-  </button>
-  <h1
-    class="bg-gradient-to-r from-sky-400 to-emerald-600 bg-clip-text text-3xl font-extrabold text-transparent md:text-5xl lg:text-6xl"
-  >
-    Profile
-  </h1>
+  <div class="flex flex-col mt-12 gap-3 max-w-xl m-auto">
+    <div class="w-full flex flex-col items-center gap-6">
+      <img
+        class="h-24 w-24 rounded-full"
+        src="https://i.pravatar.cc/300"
+        alt="Profile picture"
+      />
+      <div class="flex flex-col items-center gap-1">
+        <h1 class="text-2xl capitalize">
+          {{ user?.fullname }}
+        </h1>
+        <p class="opacity-80">{{ user?.email }}</p>
+      </div>
+    </div>
 
-  <!-- loading -->
-  <div v-if="userLoading && !user">
-    <p class="text-6xl font-black">Loading User...</p>
-  </div>
+    <!-- loading -->
+    <div v-if="userLoading && !user">
+      <p class="text-6xl font-black">Loading User...</p>
+    </div>
 
-  <!-- show user -->
-  <div v-if="!isEditingUser && user">
-    <!-- edit button -->
-    <button class="flex" @click="isEditingUser = true">
-      <Pencil class="h-6 w-6" />
-    </button>
+    <!-- show user -->
+    <div v-if="!isEditingUser && user">
+      <!-- edit button -->
+      <button class="flex" @click="isEditingUser = true">
+        <Pencil class="h-6 w-6" />
+      </button>
 
-    <div>
-      <div class="overflow-hidden rounded-lg bg-white p-6 shadow">
-        <h2 class="text-2xl font-semibold text-gray-900">
-          {{ user.fullname }}
-        </h2>
-        <p class="mt-1 text-sm text-gray-500">{{ user.email }}</p>
-        <p class="mt-2 text-sm font-medium text-gray-600">
-          Role: {{ user.role }}
-        </p>
-        <p
-          v-if="user.role !== Role.CLIENT"
-          class="mt-2 text-sm font-medium text-gray-600"
-        >
-          Absent Count: {{ user.absentCount }}
-        </p>
-        <p class="mt-2 text-sm font-medium text-gray-600" v-if="user.telephone">
-          Telephone: {{ user.telephone }}
-        </p>
-        <div v-if="user.role == Role.CLIENT && user.company">
+      <div>
+        <div class="overflow-hidden rounded-lg bg-white p-6 shadow">
+          <h2 class="text-2xl font-semibold text-gray-900">
+            {{ user.fullname }}
+          </h2>
+          <p class="mt-1 text-sm text-gray-500">{{ user.email }}</p>
           <p class="mt-2 text-sm font-medium text-gray-600">
-            Company: {{ user.company }}
+            Role: {{ user.role }}
           </p>
-          <p class="mt-2 text-sm font-medium text-gray-600">
-            Invoice Option: {{ user.invoiceOption }}
+          <p
+            v-if="user.role !== Role.CLIENT"
+            class="mt-2 text-sm font-medium text-gray-600"
+          >
+            Absent Count: {{ user.absentCount }}
           </p>
-          <p class="mt-2 text-sm font-medium text-gray-600">
-            VAT Number: {{ user.btwNumber }}
+          <p
+            class="mt-2 text-sm font-medium text-gray-600"
+            v-if="user.telephone"
+          >
+            Telephone: {{ user.telephone }}
           </p>
-        </div>
-        <!-- Locations -->
-        <div>
-          <h3 class="text-lg font-semibold text-gray-900">Locations</h3>
-          <!-- Button for adding a new location -->
-          <CustomButton
-            v-if="user.locations?.length === 0 || user.role === Role.CLIENT"
-            name="Add New Location"
-            :loading="loading.createLocation"
-            @click="openModal(null, 'create')"
-          />
-          <!-- show all locations -->
-          <div v-if="user.locations && user.locations.length > 0" class="mt-4">
+          <div v-if="user.role == Role.CLIENT && user.company">
+            <p class="mt-2 text-sm font-medium text-gray-600">
+              Company: {{ user.company }}
+            </p>
+            <p class="mt-2 text-sm font-medium text-gray-600">
+              Invoice Option: {{ user.invoiceOption }}
+            </p>
+            <p class="mt-2 text-sm font-medium text-gray-600">
+              VAT Number: {{ user.btwNumber }}
+            </p>
+          </div>
+          <!-- Locations -->
+          <div>
+            <h3 class="text-lg font-semibold text-gray-900">Locations</h3>
+            <!-- Button for adding a new location -->
+            <CustomButton
+              v-if="user.locations?.length === 0 || user.role === Role.CLIENT"
+              name="Add New Location"
+              :loading="loading.createLocation"
+              @click="openModal(null, 'create')"
+            />
             <!-- show all locations -->
-            <div v-for="location in user.locations" :key="location.id">
-              <div class="mt-4 overflow-hidden rounded-lg bg-white p-4 shadow">
-                <h3 class="text-lg font-semibold text-gray-900">
-                  {{ location.id }}
-                </h3>
-                <p class="mt-2 text-sm font-medium text-gray-600">
-                  Address: {{ location.address }}
-                </p>
-                <p class="text-gray-600">
-                  Latitude: {{ location.lat }} {{ location.lng }}
-                </p>
-                <!-- Edit and Delete buttons -->
-                <div class="mt-2 flex">
-                  <button
-                    @click="openModal(location, 'detail')"
-                    class="mr-2 rounded-md bg-green-500 px-4 py-2 text-white hover:bg-green-600"
-                  >
-                    <Eye />
-                  </button>
-                  <button
-                    @click="openModal(location, 'edit')"
-                    class="mr-2 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-                  >
-                    <Pencil />
-                  </button>
-                  <button
-                    @click="handleDeleteLocation(location.id)"
-                    class="rounded-md bg-red-500 px-4 py-2 text-white hover:bg-red-600"
-                  >
-                    <Trash2 />
-                  </button>
+            <div
+              v-if="user.locations && user.locations.length > 0"
+              class="mt-4"
+            >
+              <!-- show all locations -->
+              <div v-for="location in user.locations" :key="location.id">
+                <div
+                  class="mt-4 overflow-hidden rounded-lg bg-white p-4 shadow"
+                >
+                  <h3 class="text-lg font-semibold text-gray-900">
+                    {{ location.id }}
+                  </h3>
+                  <p class="mt-2 text-sm font-medium text-gray-600">
+                    Address: {{ location.address }}
+                  </p>
+                  <p class="text-gray-600">
+                    Latitude: {{ location.lat }} {{ location.lng }}
+                  </p>
+                  <!-- Edit and Delete buttons -->
+                  <div class="mt-2 flex">
+                    <button
+                      @click="openModal(location, 'detail')"
+                      class="mr-2 rounded-md bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+                    >
+                      <Eye />
+                    </button>
+                    <button
+                      @click="openModal(location, 'edit')"
+                      class="mr-2 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+                    >
+                      <Pencil />
+                    </button>
+                    <button
+                      @click="handleDeleteLocation(location.id)"
+                      class="rounded-md bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+                    >
+                      <Trash2 />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <CustomButton
+            name="Delete Account"
+            :loading="deleteUserLoading"
+            @click="handleDeleteUser()"
+          />
         </div>
-        <CustomButton
-          name="Delete Account"
-          :loading="deleteUserLoading"
-          @click="handleDeleteUser()"
-        />
       </div>
     </div>
-  </div>
 
-  <!-- show edit form -->
-  <div v-if="isEditingUser">
-    <!-- go back button -->
-    <button class="flex" @click="isEditingUser = false">
-      <ArrowLeft class="h-6 w-6" />
-      Go back
-    </button>
+    <!-- show edit form -->
+    <div v-if="isEditingUser">
+      <!-- go back button -->
+      <button class="flex" @click="isEditingUser = false">
+        <ArrowLeft class="h-6 w-6" />
+        Go back
+      </button>
 
-    <DynamicForm
-      :schema="formUpdateUser"
-      :validationSchema="userUpdateValidationSchema"
-      :handleForm="handleUpdateUser"
-      :loading="loading.updateUser"
-      :initial-values="{
-        firstname: user!.firstname,
-        lastname: user!.lastname,
-        email: user!.email,
-        telephone: user!.telephone,
-        invoiceOption: user!.invoiceOption,
-        company: user!.company,
-        btwNumber: user!.btwNumber,
-      }"
-    />
+      <DynamicForm
+        :schema="formUpdateUser"
+        :validationSchema="userUpdateValidationSchema"
+        :handleForm="handleUpdateUser"
+        :loading="loading.updateUser"
+        :initial-values="{
+          firstname: user!.firstname,
+          lastname: user!.lastname,
+          email: user!.email,
+          telephone: user!.telephone,
+          invoiceOption: user!.invoiceOption,
+          company: user!.company,
+          btwNumber: user!.btwNumber,
+        }"
+      />
+    </div>
   </div>
 
   <!-- Detail Location Modal -->
