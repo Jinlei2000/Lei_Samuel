dd391a1a869a2637465b6759399522849256a721
Lei <lei.jin@student.howest.be>
2023-11-26 15:36:45 +0100

    feat: make stubs users, schedules & absences

--- packages/api/src/absences/absences.service.spec.ts
+++ packages/api/src/absences/absences.service.spec.ts
@@ -7,12 +7,14 @@ import { UsersService } from 'src/users/users.service'
 import { SchedulesService } from 'src/schedules/schedules.service'
 import { CreateAbsenceInput } from './dto/create-absence.input'
 import { absenceStub, createabsenceInputStub } from './stubs/absences.stub'
-import { UsersModule } from 'src/users/users.module'
-import { SchedulesModule } from 'src/schedules/schedules.module'
+import { userStaffStub } from 'src/users/stubs/users.stub'
+import { scheduleStub } from 'src/schedules/stubs/schedules.stub'
 
 describe('AbsencesService', () => {
-  let absencesService: AbsencesService
+  let service: AbsencesService
   let mockAbsencesService: Repository<Absence>
+  let mockUsersService: UsersService
+  let mockSchedulesService: SchedulesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,44 +26,62 @@ describe('AbsencesService', () => {
             save: jest.fn().mockResolvedValue(absenceStub()),
           },
         },
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(userStaffStub()),
+            incrementAbsencesCount: jest
+              .fn()
+              .mockResolvedValue(userStaffStub()),
+          },
+        },
+        {
+          provide: SchedulesService,
+          useValue: {
+            // TODO: use schedule stub
+            updateAllByEmployeeWithDateRange: jest
+              .fn()
+              .mockResolvedValue(scheduleStub()),
+          },
+        },
       ],
     }).compile()
 
-    absencesService = module.get<AbsencesService>(AbsencesService)
+    service = module.get<AbsencesService>(AbsencesService)
     mockAbsencesService = module.get<Repository<Absence>>(
       getRepositoryToken(Absence),
     )
   })
 
   it('should be defined', () => {
-    expect(absencesService).toBeDefined()
+    expect(service).toBeDefined()
   })
 
-  // describe('create', () => {
-  //   describe('when create is called', () => {
-  //     it('should call absenceRepository.create one time', async () => {
-  //       const absenceTest = new Absence()
-  //       const saveSPy = jest.spyOn(mockAbsencesService, 'save')
-  //       await service.create(absenceTest)
-  //       expect(saveSPy).toBeCalledTimes(1)
-  //     })
+  describe('create', () => {
+    describe('when create is called', () => {
+      it('should call absenceRepository.create one time', async () => {
+        const absenceTest = new Absence()
+        const saveSPy = jest.spyOn(mockAbsencesService, 'save')
+        await service.create(absenceTest)
+        expect(saveSPy).toBeCalledTimes(1)
+      })
 
-  //     it('should call absenceRepository.save with the correct parameters', async () => {
-  //       const absenceTest: CreateAbsenceInput = createabsenceInputStub()
-  //       const saveSPy = jest.spyOn(mockAbsencesService, 'save')
+      it('should call absenceRepository.save with the correct parameters', async () => {
+        const absenceTest: CreateAbsenceInput = createabsenceInputStub()
+        const saveSPy = jest.spyOn(mockAbsencesService, 'save')
 
-  //       await service.create(absenceTest)
+        await service.create(absenceTest)
 
-  //       expect(saveSPy).toBeCalledWith(absenceTest)
-  //     })
+        expect(saveSPy).toBeCalledWith(absenceTest)
+      })
 
-  //     it('should return the created absence', async () => {
-  //       const absenceTestInput = createabsenceInputStub()
-  //       const absenceOutput = absenceStub()
+      it('should return the created absence', async () => {
+        const absenceTestInput = createabsenceInputStub()
+        const absenceOutput = absenceStub()
 
-  //       const r = await service.create(absenceTestInput)
-  //       expect(r).toEqual(absenceOutput)
-  //     })
-  //   })
-  // })
+        const r = await service.create(absenceTestInput)
+        expect(r).toEqual(absenceOutput)
+      })
+    })
+  })
 })
