d9a76f9984793ec78faf43669a757c355f543307
Lei <lei.jin@student.howest.be>
2023-11-26 14:44:13 +0100

    feat: add a create test for absences

--- packages/api/src/absences/absences.service.spec.ts
+++ packages/api/src/absences/absences.service.spec.ts
@@ -1,18 +1,67 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AbsencesService } from './absences.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { AbsencesService } from './absences.service'
+import { Absence } from './entities/absence.entity'
+import { Repository } from 'typeorm'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { UsersService } from 'src/users/users.service'
+import { SchedulesService } from 'src/schedules/schedules.service'
+import { CreateAbsenceInput } from './dto/create-absence.input'
+import { absenceStub, createabsenceInputStub } from './stubs/absences.stub'
+import { UsersModule } from 'src/users/users.module'
+import { SchedulesModule } from 'src/schedules/schedules.module'
 
 describe('AbsencesService', () => {
-  let service: AbsencesService;
+  let absencesService: AbsencesService
+  let mockAbsencesService: Repository<Absence>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AbsencesService],
-    }).compile();
+      providers: [
+        AbsencesService,
+        {
+          provide: getRepositoryToken(Absence),
+          useValue: {
+            save: jest.fn().mockResolvedValue(absenceStub()),
+          },
+        },
+      ],
+    }).compile()
 
-    service = module.get<AbsencesService>(AbsencesService);
-  });
+    absencesService = module.get<AbsencesService>(AbsencesService)
+    mockAbsencesService = module.get<Repository<Absence>>(
+      getRepositoryToken(Absence),
+    )
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(absencesService).toBeDefined()
+  })
+
+  // describe('create', () => {
+  //   describe('when create is called', () => {
+  //     it('should call absenceRepository.create one time', async () => {
+  //       const absenceTest = new Absence()
+  //       const saveSPy = jest.spyOn(mockAbsencesService, 'save')
+  //       await service.create(absenceTest)
+  //       expect(saveSPy).toBeCalledTimes(1)
+  //     })
+
+  //     it('should call absenceRepository.save with the correct parameters', async () => {
+  //       const absenceTest: CreateAbsenceInput = createabsenceInputStub()
+  //       const saveSPy = jest.spyOn(mockAbsencesService, 'save')
+
+  //       await service.create(absenceTest)
+
+  //       expect(saveSPy).toBeCalledWith(absenceTest)
+  //     })
+
+  //     it('should return the created absence', async () => {
+  //       const absenceTestInput = createabsenceInputStub()
+  //       const absenceOutput = absenceStub()
+
+  //       const r = await service.create(absenceTestInput)
+  //       expect(r).toEqual(absenceOutput)
+  //     })
+  //   })
+  // })
+})
---
+++ packages/api/src/absences/stubs/absences.stub.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from 'mongodb'
+import { CreateAbsenceInput } from '../dto/create-absence.input'
+import { Absence } from '../entities/absence.entity'
+
+export const createabsenceInputStub = (): CreateAbsenceInput => {
+  const a = new CreateAbsenceInput()
+  a.userId = '652e5989204b1d8ef65ed992'
+  a.description = 'Example description'
+  a.type = 'Example type'
+  a.startDate = new Date('2021-01-01')
+  a.endDate = new Date('2021-01-02')
+  return a
+}
+
+export const absenceStub = (): Absence => {
+  const a = new Absence()
+  a.id = new ObjectId('5f9d4a3f9d6c6a1d9c9bce1a')
+  a.userId = '652e5989204b1d8ef65ed992'
+  a.description = 'Example description'
+  a.type = 'Example type'
+  a.startDate = new Date('2021-01-01')
+  a.endDate = new Date('2021-01-02')
+  a.totalDays = 2
+  return a
+}
