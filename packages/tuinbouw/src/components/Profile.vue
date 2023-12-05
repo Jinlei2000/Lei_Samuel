f2d2c61f845a252698a5f6543a6e8f2911189d8f
Lei <lei.jin@student.howest.be>
2023-12-05 14:36:48 +0100

    feat: add edit

--- packages/tuinbouw/src/components/Profile.vue
+++ packages/tuinbouw/src/components/Profile.vue
@@ -7,11 +7,32 @@
 
     <!-- Primary user info -->
     <div class="flex w-full flex-col items-center gap-6">
-      <img
-        class="h-24 w-24 rounded-full"
-        src="https://i.pravatar.cc/300"
-        alt="Profile picture"
-      />
+      <div class="relative">
+        <div>
+          <img
+            v-if="user?.url"
+            class="h-24 w-24 rounded-full"
+            :src="user?.url"
+            alt="Profile picture"
+          />
+          <div
+            v-else
+            class="bg-primary-green flex h-24 w-24 items-center justify-center rounded-full"
+          >
+            <p class="text-4xl text-white">
+              {{ user?.firstname[0].toUpperCase() }}
+            </p>
+          </div>
+        </div>
+        <button
+          class="bg-primary-orange absolute right-0 top-0 h-6 w-6 rounded-full"
+          @click="toggleUserModal('editPicture')"
+        >
+          <Edit2
+            class="absolute left-1/2 top-1/2 flex h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform items-center justify-center stroke-white"
+          />
+        </button>
+      </div>
       <div class="flex flex-col items-center gap-1">
         <h1 class="text-2xl capitalize">
           {{ user?.fullname }}
@@ -533,6 +554,79 @@
       </div>
     </form>
   </Dialog>
+
+  <!-- Edit Profile  -->
+  <Dialog
+    v-model:visible="userModalVisible.editPicture"
+    modal
+    header="Edit Profile Picture"
+    :draggable="false"
+    :close-on-escape="true"
+    :pt="{
+      root: {
+        class: 'w-full mx-3 md:m-0 md:max-w-lg',
+      },
+    }"
+  >
+    <div class="p-4">
+      <label for="uploadInput" class="mb-2 block font-bold text-gray-700"
+        >Upload Image</label
+      >
+      <div class="flex w-full items-center justify-center">
+        <label
+          for="dropzone-file"
+          class="dark:hover:bg-bray-800 flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        >
+          <div class="flex flex-col items-center justify-center pb-6 pt-5">
+            <svg
+              class="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 16"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+              />
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              <span class="font-semibold">Click to upload</span> or drag and
+              drop
+            </p>
+          </div>
+          <input
+            id="dropzone-file"
+            type="file"
+            class="hidden"
+            accept="image/*"
+            @change="
+              $event => {
+                const target = $event.target as HTMLInputElement
+                selectedPicture = (target.files as FileList)[0]
+              }
+            "
+          />
+        </label>
+      </div>
+
+      <div class="mt-6 flex justify-between">
+        <CustomButton
+          name="Cancel"
+          variant="secondary"
+          @click="toggleUserModal()"
+        />
+        <CustomButton
+          name="Upload"
+          variant="primary"
+          @click="handleUploadImage"
+        />
+      </div>
+    </div>
+  </Dialog>
 </template>
 
 <script setup lang="ts">
@@ -580,7 +674,7 @@ import { useRouter } from 'vue-router'
 const { customUser } = useCustomUser()
 const { showToast } = useCustomToast()
 const { formatDateTime } = useTimeUtilities()
-const { firebaseUser } = useFirebase()
+const { firebaseUser, uploadProfile } = useFirebase()
 const { replace } = useRouter()
 const { searchAddress } = useTomTomMap()
 
@@ -609,6 +703,10 @@ const locationModalVisible = ref({
 
 // variables
 const isEditingUser = ref<boolean>(false)
+const selectedPicture = ref<File | null>(null)
+const userModalVisible = ref({
+  editPicture: false,
+})
 const user = computed<CustomUser | null>(() => userResult.value?.user || null)
 const selectedLocation = ref<Location | null>(null)
 const searchAddressResults = ref<Location[] | null>(null)
@@ -965,6 +1063,30 @@ const { mutate: deleteLocation, error: deleteLocationError } =
   useMutation(DELETE_LOCATION)
 
 // logics
+// handle upload image
+const handleUploadImage = async () => {
+  try {
+    // save image to firebase & get url
+    const url = await uploadProfile(
+      customUser.value!.uid,
+      selectedPicture.value as File,
+    )
+    // update user in db
+    await updateUser({
+      updateUserInput: {
+        id: customUser.value?.id,
+        url: url,
+      },
+    })
+
+    showToast('success', 'Success', 'Profile picture has been updated')
+    toggleUserModal()
+  } catch (error) {
+    console.log(error)
+    if (error instanceof Error) showToast('error', 'Error', error.message)
+  }
+}
+
 // handle delete user
 const handleDeleteUser = async () => {
   await deleteUser({
@@ -1131,6 +1253,26 @@ const toggleLocationModal = (
         edit: false,
         create: false,
       }
+      break
+    default:
+      break
+  }
+}
+
+const toggleUserModal = (type: string = 'close') => {
+  console.log(type)
+  switch (type) {
+    case 'editPicture':
+      userModalVisible.value = {
+        editPicture: true,
+      }
+      console.log(userModalVisible.value)
+      break
+    case 'close':
+      userModalVisible.value = {
+        editPicture: false,
+      }
+      selectedPicture.value = null
       break
     default:
       break
