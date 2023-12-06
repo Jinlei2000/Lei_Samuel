eba2f5f3605a9e3589c85fe36398371a9f04c807
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-12-06 10:40:43 +0100

    feat: setup base for testing

--- packages/api/src/materials/materials.service.spec.ts
+++ packages/api/src/materials/materials.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { MaterialsService } from './materials.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Material } from './entities/material.entity'
+import { CreateMaterialInput } from './dto/create-material.input'
 
 describe('MaterialsService', () => {
   let service: MaterialsService
+  let mockMaterialsRepository: Repository<Material>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MaterialsService],
+      providers: [
+        MaterialsService,
+        {
+          provide: getRepositoryToken(Material),
+          useValue: {
+            save: jest.fn().mockResolvedValue(new Material()),
+            find: jest.fn().mockResolvedValue([new Material()]),
+            findOne: jest.fn().mockResolvedValue(new Material()),
+          },
+        },
+      ],
     }).compile()
 
     service = module.get<MaterialsService>(MaterialsService)
+    mockMaterialsRepository = module.get<Repository<Material>>(
+      getRepositoryToken(Material),
+    )
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('create', () => {
+    let materialTestInput: CreateMaterialInput
+    let materialResult: Material
+
+    beforeEach(async () => {
+      jest.spyOn(mockMaterialsRepository, 'find').mockResolvedValueOnce([])
+
+      materialTestInput = new CreateMaterialInputStub()
+      materialResult = await service.create(materialTestInput)
+    })
   })
 })
