5fc2e34c5a0e7335a8b6963e0c0c89f0f512b226
Lei <lei.jin@student.howest.be>
2023-12-06 19:46:00 +0100

    feat: refactor upload image when click

--- packages/tuinbouw/src/components/Profile.vue
+++ packages/tuinbouw/src/components/Profile.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="m-auto my-12 flex max-w-xl flex-col gap-6">
+  <div v-if="user" class="m-auto my-12 flex max-w-xl flex-col gap-6">
     <!-- Primary user info -->
     <div class="flex w-full flex-col items-center gap-6">
       <div class="relative">
+        <!-- Profile picture -->
         <div>
           <img
             v-if="user?.url"
@@ -19,15 +20,49 @@
             </p>
           </div>
         </div>
-        <button
+        <!-- Edit profile picture -->
+        <label
           class="bg-primary-orange absolute right-0 top-0 h-6 w-6 rounded-full"
-          @click="toggleUserModal('editPicture')"
+          for="upload-image"
+          tabindex="0"
         >
-          <Edit2
-            class="absolute left-1/2 top-1/2 flex h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform items-center justify-center stroke-white"
-          />
-        </button>
+          <div v-if="!loadingUser.uploadPicture">
+            <Edit2
+              class="absolute left-1/2 top-1/2 flex h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform items-center justify-center stroke-white"
+            />
+            <input
+              id="upload-image"
+              type="file"
+              class="hidden"
+              accept="image/*"
+              @change="$event => handleUploadImage($event)"
+            />
+          </div>
+          <div
+            v-else
+            class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform stroke-white"
+          >
+            <svg
+              aria-hidden="true"
+              role="status"
+              class="h-3 w-3 animate-spin text-white"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="#E5E7EB"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentColor"
+              />
+            </svg>
+          </div>
+        </label>
       </div>
+      <!-- Name & Email -->
       <div class="flex flex-col items-center gap-1">
         <h1 class="text-2xl capitalize">
           {{ user?.fullname }}
@@ -37,12 +72,12 @@
     </div>
 
     <!-- About me -->
-    <div v-if="user && !isEditingUser" class="w-full">
+    <div class="w-full">
       <div class="mb-3 flex w-full justify-between">
         <h2 class="text-2xl">About me</h2>
         <button
           class="text-primary-orange flex items-center gap-2 text-lg"
-          @click="isEditingUser = true"
+          @click="toggleUserModal('update')"
         >
           <Edit2 class="h-5 w-5" /> Edit
         </button>
@@ -61,31 +96,6 @@
           </p>
         </li>
       </ul>
-    </div>
-
-    <!-- Edit About me -->
-    <div v-if="isEditingUser">
-      <!-- go back button -->
-      <button class="flex" @click="isEditingUser = false">
-        <ArrowLeft class="h-6 w-6" />
-        Go back
-      </button>
-
-      <DynamicForm
-        :schema="formUpdateUser"
-        :validation-schema="userUpdateValidationSchema"
-        :handle-form="handleUpdateUser"
-        :loading="loadingUser.update"
-        :initial-values="{
-          firstname: user!.firstname,
-          lastname: user!.lastname,
-          email: user!.email,
-          telephone: user!.telephone,
-          invoiceOption: user!.invoiceOption,
-          company: user!.company,
-          btwNumber: user!.btwNumber,
-        }"
-      />
     </div>
 
     <!-- Absences -->
@@ -133,7 +143,7 @@
     </div>
 
     <!-- Locations -->
-    <div v-if="user" class="flex w-full flex-col gap-3">
+    <div class="flex w-full flex-col gap-3">
       <h2 class="text-2xl">Locations</h2>
       <button
         v-if="
@@ -181,7 +191,7 @@
       </div>
     </div>
 
-    <!-- delete account -->
+    <!-- Delete Account -->
     <button
       class="bg-primary-red rounded-2xl py-3 text-white"
       @click="handleDeleteUser()"
@@ -189,6 +199,38 @@
       Delete Account
     </button>
   </div>
+
+  <!-- TODO: show skeleton -->
+
+  <!-- Update User Modal -->
+  <Dialog
+    v-model:visible="userModalVisible.update"
+    modal
+    header="Update User"
+    :draggable="false"
+    :close-on-escape="true"
+    :pt="{
+      root: {
+        class: 'w-full mx-3 md:m-0 md:max-w-lg',
+      },
+    }"
+  >
+    <DynamicForm
+      :schema="formUpdateUser"
+      :validation-schema="userUpdateValidationSchema"
+      :handle-form="handleUpdateUser"
+      :loading="loadingUser.update"
+      :initial-values="{
+        firstname: user!.firstname,
+        lastname: user!.lastname,
+        email: user!.email,
+        telephone: user!.telephone,
+        invoiceOption: user!.invoiceOption,
+        company: user!.company,
+        btwNumber: user!.btwNumber,
+      }"
+    />
+  </Dialog>
 
   <!-- Detail Absence Modal -->
   <Dialog
@@ -523,81 +565,6 @@
       </div>
     </form>
   </Dialog>
-
-  <!-- Edit Profile Picture  -->
-  <Dialog
-    v-model:visible="userModalVisible.editPicture"
-    modal
-    header="Upload Image"
-    :draggable="false"
-    :close-on-escape="true"
-    :pt="{
-      root: {
-        class: 'w-full mx-3 md:m-0 md:max-w-lg',
-      },
-    }"
-  >
-    <form class="p-4" @submit.prevent="handleUploadImage">
-      <div class="flex w-full flex-col">
-        <label
-          for="dropzone-file"
-          class="dark:hover:bg-bray-800 flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-        >
-          <div class="flex flex-col items-center justify-center pb-6 pt-5">
-            <svg
-              class="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 16"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-              />
-            </svg>
-            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span class="font-semibold">Click to upload</span> or drag and
-              drop
-            </p>
-          </div>
-          <input
-            id="dropzone-file"
-            type="file"
-            class="hidden"
-            accept="image/*"
-            @change="
-              $event => {
-                const target = $event.target as HTMLInputElement
-                selectedPicture = (target.files as FileList)[0]
-              }
-            "
-          />
-        </label>
-
-        <span class="block text-sm text-red-500">
-          {{ errorMessageSelectedPicture }}
-        </span>
-      </div>
-
-      <div class="mt-6 flex justify-between">
-        <CustomButton
-          name="Cancel"
-          variant="secondary"
-          @click="toggleUserModal()"
-        />
-        <CustomButton
-          type="submit"
-          :loading="loadingUpload"
-          name="Upload"
-          variant="primary"
-        />
-      </div>
-    </form>
-  </Dialog>
 </template>
 
 <script setup lang="ts">
@@ -636,7 +603,7 @@ import {
 } from '@/validation/schema'
 import { useLazyQuery, useMutation, useQuery } from '@vue/apollo-composable'
 import LogRocket from 'logrocket'
-import { ArrowLeft, Edit2, PlusCircle } from 'lucide-vue-next'
+import { Edit2, PlusCircle } from 'lucide-vue-next'
 import { Form, useForm } from 'vee-validate'
 import { computed, onMounted, ref, watchEffect } from 'vue'
 import { useRouter } from 'vue-router'
@@ -649,28 +616,17 @@ const { firebaseUser, uploadProfile, deleteProfile } = useFirebase()
 const { replace } = useRouter()
 const { searchAddress } = useTomTomMap()
 
-// variables
-const variables = ref<VariablesProps>({
-  filters: [],
-  order: {
-    field: 'createdAt',
-    direction: ORDER_DIRECTION.DESC,
-  },
-})
-
 //#region USER
-const isEditingUser = ref<boolean>(false)
-const selectedPicture = ref<File | null>(null)
 const userModalVisible = ref({
-  editPicture: false,
+  update: false,
 })
 const user = computed<CustomUser | null>(() => userResult.value?.user || null)
-const loadingUpload = ref<boolean>(false)
-const errorMessageSelectedPicture = ref<string>('')
 const loadingUser = ref<{
   update: boolean
+  uploadPicture: boolean
 }>({
   update: false,
+  uploadPicture: false,
 })
 
 // form schema update user
@@ -749,19 +705,14 @@ const { mutate: deleteUser, error: deleteUserError } = useMutation(DELETE_USER)
 
 // logics
 // handle upload image
-const handleUploadImage = async () => {
-  errorMessageSelectedPicture.value = ''
-  loadingUpload.value = true
-  if (!selectedPicture.value) {
-    errorMessageSelectedPicture.value = 'Please select a picture'
-    loadingUpload.value = false
-    return
-  }
+const handleUploadImage = async (event: Event) => {
+  loadingUser.value.uploadPicture = true
+  const selectedPicture = (event.target as HTMLInputElement).files?.[0]
   try {
     // save image to firebase & get url
     const url = await uploadProfile(
       customUser.value!.uid,
-      selectedPicture.value as File,
+      selectedPicture as File,
     )
     // update user in db
     await updateUser({
@@ -771,7 +722,7 @@ const handleUploadImage = async () => {
       },
     })
 
-    loadingUpload.value = false
+    loadingUser.value.uploadPicture = false
     showToast('success', 'Success', 'Profile picture has been updated')
     toggleUserModal()
   } catch (error) {
@@ -820,19 +771,28 @@ const handleUpdateUser = async (values: CustomUser) => {
 }
 
 const toggleUserModal = (type: string = 'close') => {
+  //   const toggleModal = (
+  //   absence: Absence | null = null,
+  //   type: string = 'close',
+  // ) => {
+  //   selectedAbsence.value = absence ? { ...absence } : null
+  //   visible.value = {
+  //     detail: type === 'detail',
+  //     edit: type === 'edit',
+  //     create: type === 'create',
+  //   }
+  // }
+
   switch (type) {
-    case 'editPicture':
+    case 'update':
       userModalVisible.value = {
-        editPicture: true,
+        update: true,
       }
-      selectedPicture.value = null
-      errorMessageSelectedPicture.value = ''
       break
     case 'close':
       userModalVisible.value = {
-        editPicture: false,
+        update: false,
       }
-
       break
     default:
       break
@@ -849,6 +809,7 @@ watchEffect(() => {
     if (error) {
       loadingUser.value = {
         update: false,
+        uploadPicture: false,
       }
 
       showToast('error', 'Error', error.message)
@@ -870,6 +831,14 @@ const absenceModalVisible = ref({
 const absences = computed<Absence[]>(
   () => absencesByUserIdResult.value?.absencesByUserId || [],
 )
+
+const variables = ref<VariablesProps>({
+  filters: [],
+  order: {
+    field: 'createdAt',
+    direction: ORDER_DIRECTION.DESC,
+  },
+})
 
 const loadingAbsence = ref<{
   create: boolean
