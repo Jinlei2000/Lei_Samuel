6ca85a4d39c332a578ea1253ab5e7e0b78aae063
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-12-17 13:51:55 +0100

    chore: cleanup

--- packages/tuinbouw/src/views/client/Dashboard.vue
+++ packages/tuinbouw/src/views/client/Dashboard.vue
@@ -1,22 +1,5 @@
 <template>
-  <div class="flex flex-col items-center justify-center">
-    <h1
-      class="mb-5 bg-gradient-to-r from-sky-400 to-emerald-600 bg-clip-text text-3xl font-extrabold text-transparent md:text-5xl lg:text-6xl"
-    >
-      test
-    </h1>
-    <!-- make a list of buttons go to the right page -->
-    <div class="flex gap-2">
-      <Router-link
-        v-for="b in ['add-appointment', 'appointments']"
-        :key="b"
-        class="focus:shadow-outline-red mt-4 rounded-lg border border-transparent bg-red-600 px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 hover:bg-red-700 focus:outline-none active:bg-red-600"
-        :to="`/client/${b}`"
-        >{{ b }}</Router-link
-      >
-    </div>
-  </div>
-  <div class="m-auto flex max-w-xl flex-col gap-3">
+  <div class="m-auto mt-12 flex max-w-xl flex-col gap-3 md:mt-24">
     <h1 class="text-xl">Welkom, {{ customUser!.firstname }}</h1>
     <RouterLink to="/client/add-appointment">
       <button
@@ -49,15 +32,6 @@
       </div>
     </div>
   </div>
-  <!-- TODO: show map with al your location -->
-  <!-- TODO: button card to go to all appointments -->
-  <!-- TODO: button card to go to add appointment -->
-  <!-- TODO: show number of amount of appointments -->
-  <!-- TODO: show number of completed appointments -->
-  <!-- TODO: show number of appointments is scheduled -->
-  <!-- TODO: show number of appointments is not scheduled -->
-  <!-- TODO: show number of appointments is not completed -->
-  <!-- TODO: show appointments that has to rescheduled -->
 </template>
 
 <script setup lang="ts">
