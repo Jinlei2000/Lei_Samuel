05df91a18c82c44bc6ce84c19a0c89e5204b4f31
Lei <lei.jin@student.howest.be>
2023-12-20 10:38:41 +0100

    feat: translate

--- packages/tuinbouw/src/views/client/Dashboard.vue
+++ packages/tuinbouw/src/views/client/Dashboard.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="m-auto mt-12 flex max-w-xl flex-col gap-3 md:mt-24">
-    <h1 class="text-xl">Welkom, {{ customUser!.firstname }}</h1>
+    <h1 class="text-xl">
+      {{ $t('dashboard.client.welcome') }} {{ customUser!.firstname }}
+    </h1>
     <RouterLink to="/client/add-appointment">
       <button
         class="border-primary-green text-primary-green flex h-16 w-full items-center justify-center rounded-2xl border-[1px]"
       >
         <PlusCircle class="mr-2" />
-        Add New Appointment
+        {{ $t('dashboard.client.button.add') }}
       </button>
     </RouterLink>
     <div class="flex w-full items-center justify-between">
-      <p class="text-xl">Upcoming appointments</p>
+      <p class="text-xl">
+        {{ $t('dashboard.client.upcoming') }}
+      </p>
       <RouterLink
         to="/client/appointments"
         class="text-primary-orange group flex items-center text-lg sm:text-base"
       >
-        All appointments
+        {{ $t('dashboard.client.button.all') }}
         <ChevronRight
           class="h-8 w-8 transition-all group-hover:translate-x-1 sm:h-auto sm:w-auto"
           stroke-width="1"
@@ -37,47 +41,38 @@
 <script setup lang="ts">
 import useCustomUser from '../../composables/useCustomUser'
 import AppointmentCard from '@/components/generic/AppointmentCard.vue'
-import {
-  GET_RECENT_APPOINTMENTS_BY_USERID,
-  GET_UPCOMING_APPOINTMENTS_BY_USERID,
-} from '@/graphql/appointment.query'
+import useCustomToast from '@/composables/useCustomToast'
+import { GET_UPCOMING_APPOINTMENTS_BY_USERID } from '@/graphql/appointment.query'
 import type { Appointment } from '@/interfaces/appointment.user.interface'
-import { useLazyQuery, useQuery } from '@vue/apollo-composable'
+import { useQuery } from '@vue/apollo-composable'
+import LogRocket from 'logrocket'
 import { ChevronRight, PlusCircle } from 'lucide-vue-next'
 import { type ComputedRef, watchEffect } from 'vue'
 import { computed } from 'vue'
 
 const { customUser } = useCustomUser()
+const { showToast } = useCustomToast()
 
 const upcomingAppointments: ComputedRef<Appointment[]> = computed(() => {
   return upcomingAppointmentsResult.value?.appointmentsUpcomingByUserId || []
 })
 
-const recentAppointments: ComputedRef<Appointment[]> = computed(() => {
-  return recentAppointmentsResult.value?.appointmentsRecentByUserId || []
-})
-
 // graphql
-const {
-  result: upcomingAppointmentsResult,
-  error: upcomingAppointmentsError,
-  loading: upcomingAppointmentsLoading,
-} = useQuery(GET_UPCOMING_APPOINTMENTS_BY_USERID, {
-  userId: customUser?.value?.id,
-  amount: 5,
-  fetchPolicy: 'cache-and-network',
-})
-
-const {
-  result: recentAppointmentsResult,
-  loading: recentAppointmentsLoading,
-  error: recentAppointmentsError,
-} = useQuery(GET_RECENT_APPOINTMENTS_BY_USERID, () => ({
-  userId: customUser.value?.id,
-  amount: 5,
-}))
+const { result: upcomingAppointmentsResult, error: upcomingAppointmentsError } =
+  useQuery(GET_UPCOMING_APPOINTMENTS_BY_USERID, {
+    userId: customUser?.value?.id,
+    amount: 5,
+    fetchPolicy: 'cache-and-network',
+  })
 
 watchEffect(() => {
-  console.log(upcomingAppointmentsResult.value)
+  // log the queries
+
+  // all errors
+  if (upcomingAppointmentsError.value) {
+    // console.log(upcomingAppointmentsError.value)
+    LogRocket.captureException(upcomingAppointmentsError.value)
+    showToast('error', 'toast.error', 'dashboard.client.toast.error.upcoming')
+  }
 })
 </script>
